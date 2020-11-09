884091451bb48cf8835b166c6b9bf51adb0a4465
MAUREEN <maureennayebare2@gmail.com>
2020-11-09 12:07:45 +0300

    Pop

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log('Hello World!');
+//This is the alert Pop-up box which works without the windows prefix. //
+alert('Hello world');
+//It displays a pop-up box with a message and OK and Cancel buttons. //
+//if(confirm('Do you love music? ')){alert('You pressed OK. Thank you');}
+//else{alert('You pressed cancel. ');}
+//console.log(alerted);//
+//Displays a pop up box with a message asking the user for a text input. It returns a string containing the text entered by the user//
+var firstName=prompt('What is your first Name? ');
+if(firstName==''||firstName==null){alert('You did not enter a value.');}
+else{alert('Hi'+firstName+', always remember that God loves you');}
+console.log(alerted);
